9cec16a148a2dd9884310335e978bd3ec6d13bad
三千 <821923589@qq.com>
2025-12-15 22:42:56 +0800

    🐛 更新错误类型结构体和错误处理逻辑
    添加了 ErrorCode 和 ErrorMsg 字段来增强错误信息的表达能力，并修改了 IsError 方法以包含 ErrorCode 的检查，同时在 Error 方法中增加了对 ErrorMsg 的优先级处理，确保更详细的错误信息能够被正确返回。
    
    这样修改使得错误处理更加完善，可以区分不同类型的错误并提供更有意义的错误信息。

--- types/error.go
+++ types/error.go
@@ -1,6 +1,8 @@
 package types
 
 type Error struct {
+	ErrorCode    int    `json:"error_code"`
+	ErrorMsg     string `json:"error_msg"`
 	AuthError    string `json:"error"`
 	AuthErrorMsg string `json:"error_description"`
 	Errno        int    `json:"errno"`
@@ -9,12 +11,15 @@ type Error struct {
 }
 
 func (e Error) IsError() bool {
-	return !(e.AuthError == "" && e.Errno == 0)
+	return !(e.AuthError == "" && e.Errno == 0 && e.ErrorCode == 0)
 }
 
 func (e Error) Error() string {
 	if e.AuthErrorMsg != "" {
 		return e.AuthErrorMsg
+	}
+	if e.ErrorMsg != "" {
+		return e.ErrorMsg
 	}
 	return e.ErrMsg
 }
